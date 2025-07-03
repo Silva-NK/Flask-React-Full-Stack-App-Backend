f433507a4460f9aff3c32c9c973467e7049fb778
Silva-NK <kiagonsuzanne@gmail.com>
2025-07-03 15:06:43 +0300

    Changes for deployment...

--- server/app.py
+++ server/app.py
@@ -24,6 +24,7 @@ def after_request(response):
     response.headers["Access-Control-Allow-Origin"] = "https://event-manager-flask-react-app.onrender.com"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
     return response
 
 
